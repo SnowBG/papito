10e9bea8d0f5f67f7ee7f244402ba558b36393f5
snowbg <snowbg@gmail.com>
2024-11-22 13:34:46 -0300

    Adicionados os testes de falha de e-mail e senha.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,45 @@
 describe('Login', () => {
   it('Logar com sucesso', () => {
     cy.visit('https://playground.cyskills.com.br')
+
+    cy.contains('h2', 'Faça login').should('be.visible')
     cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
     cy.get('[data-cy="password"]').type('showtime')
     cy.get('[data-cy="login-button"]').click()
 
     cy.get('[data-cy="welcome-title"]').should('be.visible').and('have.text', 'Boas vindas ao Cypress Playground')
+    
+    
   })
+
+
+  it('Não deve logar com senha inválida', () => {
+    cy.visit('https://playground.cyskills.com.br')
+
+    cy.contains('h2', 'Faça login').should('be.visible')
+    cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
+    cy.get('[data-cy="password"]').type('showtkime')
+    cy.get('[data-cy="login-button"]').click()
+
+    cy.get('.notice p')
+      .should('be.visible')
+      .and('have.text', 'E-mail ou senha incorretos. Por favor, tente novamente!')
+    
+    
+  })
+})
+
+it.only('Não deve logar com senha inválida', () => {
+  cy.visit('https://playground.cyskills.com.br')
+
+  cy.contains('h2', 'Faça login').should('be.visible')
+  cy.get('[data-cy="email"]').type('teste@cyskills.com.br')
+  cy.get('[data-cy="password"]').type('showtime')
+  cy.get('[data-cy="login-button"]').click()
+
+  cy.get('.notice p')
+    .should('be.visible')
+    .and('have.text', 'E-mail ou senha incorretos. Por favor, tente novamente!')
+  
+  
 })
