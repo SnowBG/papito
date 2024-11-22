f5568fa61025562f872a771c9545593dd8c17714
snowbg <snowbg@gmail.com>
2024-11-22 12:05:54 -0300

    Complete login test.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,10 @@
 describe('Login', () => {
   it('Logar com sucesso', () => {
     cy.visit('https://playground.cyskills.com.br')
-    cy.get('[data-cy="email"]').
+    cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
+    cy.get('[data-cy="password"]').type('showtime')
+    cy.get('[data-cy="login-button"]').click()
+
+    cy.get('[data-cy="welcome-title"]').should('be.visible').and('have.text', 'Boas vindas ao Cypress Playground')
   })
 })
